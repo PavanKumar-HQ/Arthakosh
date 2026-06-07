fe1d1c05687fc2417db89f02063f29b5d0782a98
Pavan Kumar S <sumapavan1231@gmail.com>
2026-06-07 21:06:47 +0530

    feat: embed youtube background music starting at 8s for Meghana's journey

--- app/meghana/page.tsx
+++ app/meghana/page.tsx
@@ -13,6 +13,14 @@ import { FestiveBackground } from "@/components/FestiveBackground";
 export default function MeghanaJourney() {
   return (
     <main className="bg-transparent text-white min-h-screen selection:bg-amber-500/30 relative">
+      <iframe
+        width="1"
+        height="1"
+        src="https://www.youtube.com/embed/fUffhENQ0Oo?autoplay=1&start=8&controls=0&showinfo=0&autohide=1&loop=1&playlist=fUffhENQ0Oo"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        className="absolute opacity-0 pointer-events-none w-1 h-1 z-[-1]"
+      />
       <FestiveBackground />
       <div className="relative z-10">
       <OpeningSequence />
